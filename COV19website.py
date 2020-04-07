9faaaf286b328f81154ac96bf4ccb1bb532090ce
umair akram <amianumair@gmail.com>
2020-04-07 21:24:50 +0500

    requirementsAdded

--- COV19website.py
+++ COV19website.py
@@ -11,15 +11,5 @@ def home():
     return render_template('index.html',data=data)
 
 
-@app.route('/about')
-def about():
-    return render_template('about.html',title='About')
-
-
-
-
-
-
-
 if __name__ == "__main__":
         app.run(debug=True) 
