e5feee8f414739fd052388f35e75af42e272ad90
umair akram <amianumair@gmail.com>
2020-04-08 13:08:33 +0500

    FixingError

--- COV19website.py
+++ COV19website.py
@@ -5,8 +5,8 @@ app = Flask(__name__)
 data=getData()
 print("data yah h ",data)
 
-@app.route('/')
-@app.route('/home')
+@app.route('carona-virus-website.herokuapp.com')
+@app.route('carona-virus-website.herokuapp.com/home')
 def home():
     return render_template('index.html',data=data)
 
